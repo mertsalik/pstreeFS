2fce9795e0894d7b9d331afc41edac7f4213c261
Mert Salık <mertsalik.itu@gmail.com>
2013-12-23 06:39:21 +0200

    added parser to library

--- src/lib/proc.h
+++ src/lib/proc.h
@@ -82,3 +82,37 @@ void read_proc_list(node* head){
 		closedir(dfd);
 	}
 }
+
+/**
+ *	If path is not correct
+ *	It returns -1
+ */
+void parse_path(char *path, char *result){
+	char *pch;
+	char tmp[20];
+	char *saveptr;
+	pch = strtok_r(path, "/", &saveptr);
+	while(pch!=NULL){
+		if(strcmp(pch,"0")==0){
+			strncpy(tmp,pch,strlen(pch)+1);
+			pch = strtok_r(NULL, "/", &saveptr);
+			continue;
+		}
+		char stat[2048];
+		char pid[20];
+		strncpy(pid,pch,strlen(pch)+1);
+		read_proc_stat(pid,stat);
+		char ppid[20];
+		get_ppid_from_stat(stat,ppid);
+		if(strcmp(ppid,tmp)==0){
+			strncpy(tmp,pch,strlen(pch)+1);
+			pch = strtok_r(NULL, "/", &saveptr);
+		}else{
+			strcpy(tmp,"-1");
+			break;
+		}
+		
+		
+	}
+	strncpy(result,tmp,strlen(tmp)+1);
+}
