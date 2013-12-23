faf3bc34e3a27bdce5248bbc6bed497ee71223bb
Mert Salık <mertsalik.itu@gmail.com>
2013-12-23 04:56:10 +0200

    implementing lib functions

---
+++ src/lib/proc.h
@@ -0,0 +1,16 @@
+#include <string.h>
+
+void get_name_of_proc(char *pid, char *result_buf){
+        char cmd[1000];
+        sprintf(cmd,"/proc/%s/cmdline",pid);
+        //printf("%s",cmd);
+        FILE *cmdline = fopen(cmd,"rb");
+        char *arg = 0;
+        size_t size = 0;
+        while(getdelim(&arg, &size, 0, cmdline) != -1){
+                //puts(arg);
+        }
+        strncpy(result_buf, arg, size+1);
+        free(arg);
+        fclose(cmdline);
+}
