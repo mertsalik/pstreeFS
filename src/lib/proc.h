42a3b88513615004847f75dedd5291b8ed0ef3d2
Mert Salık <mertsalik.itu@gmail.com>
2013-12-23 05:07:03 +0200

    updated library

--- src/lib/proc.h
+++ src/lib/proc.h
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <string.h>
 
 void get_name_of_proc(char *pid, char *result_buf){
@@ -13,4 +16,69 @@ void get_name_of_proc(char *pid, char *result_buf){
         strncpy(result_buf, arg, size+1);
         free(arg);
         fclose(cmdline);
+}
+
+void read_proc_stat(char *pid, char *buf){
+	char cmd[2048];
+	sprintf(cmd,"/proc/%s/stat",pid);
+	FILE *stat = fopen(cmd,"rb");
+	char *arg = 0;
+	size_t size = 0;
+	while(getdelim(&arg, &size, 0, stat) != -1){
+		//puts(arg);
+	}
+	strncpy(buf, arg, size+1);
+	free(arg);
+	fclose(stat);
+}
+
+/**
+ * Returns 4. item on STAT FILE
+ */
+void get_ppid_from_stat(char *stat_info, char *buf){
+	int i = 0;
+	char *pch;
+	pch = strtok(stat_info, " ");
+	while(pch!=NULL){
+		if(i==3){
+			break;
+		}
+		pch = strtok(NULL, " ");
+		i++;
+	}
+	strncpy(buf,pch,strlen(pch)+1);
+}
+
+typedef struct node{
+	char name[2048];
+	struct node *next;
+}node;
+
+int is_proc_folder(char *dname){
+	int i ;
+	for(i=0;i<strlen(dname);i++){
+		if(!isdigit(dname[i])){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void read_proc_list(node* head){
+	node* temp;
+	temp = head;
+	struct dirent *dp;
+	DIR *dfd = opendir("/proc");
+	if(dfd != NULL) {
+		while((dp = readdir(dfd)) != NULL){
+			if(is_proc_folder(dp->d_name)){
+				strncpy(temp->name,dp->d_name,strlen(dp->d_name)+1);
+				node* nnode = (struct node*) malloc(sizeof(struct node));
+				nnode->next = NULL;
+				temp->next = nnode;
+				temp = nnode;
+			}
+		}
+		closedir(dfd);
+	}
 }
