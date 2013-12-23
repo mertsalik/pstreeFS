4b9bb29c3a8fd036b133ec0da1c84470d4934e78
Mert Salık <mertsalik.itu@gmail.com>
2013-12-23 02:59:02 +0200

    can read cmdline now

--- src/helpers/read_cmdline.c
+++ src/helpers/read_cmdline.c
@@ -1,15 +1,27 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv){
-	FILE *cmdline = fopen("/proc/1/cmdline","rb");
+void get_name_of_proc(char *pid, char *result_buf){
+	char cmd[1000];
+	sprintf(cmd,"/proc/%s/cmdline",pid);
+	//printf("%s",cmd);
+	FILE *cmdline = fopen(cmd,"rb");
 	char *arg = 0;
 	size_t size = 0;
 	while(getdelim(&arg, &size, 0, cmdline) != -1){
-		puts(arg);
+		//puts(arg);
 	}
+	strncpy(result_buf, arg, size+1);
 	free(arg);
 	fclose(cmdline);
+}
+
+
+int main(int argc, char **argv){
+	char result[200];
+	get_name_of_proc("1", result);
+	printf("%s\n",result);
 	return 0;
 }
